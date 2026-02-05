b7b7383e21b594a3c60d577a31c557937389bf0c
MarekPlasek <marek.plasek.s@gyarab.cz>
2026-02-05 23:15:33 +0100

    completed homework: exchange

--- kurzy/exchange.py
+++ kurzy/exchange.py
@@ -1,10 +1,32 @@
 #!/usr/bin/env python3
-import httpx
+import httpx, sys
 
 cnb_url = "https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt"
 
 r = httpx.get(cnb_url)
-print(r.text)
-print(r.status)
 lines = r.text.split('\n')
-print(lines[0])
+
+line_euro = ""
+
+for line in lines:
+	if "EUR" in line:
+		line_euro = line
+		break
+
+rate_str = line_euro.split('|')[-1].replace(',', '.')
+rate = float(rate_str)
+
+print("Kurz eura je", rate)
+print("\033[1mCurrency exchange thingy\033[0m")
+
+choice = input("What currency ya wanna convert? [EUR/CZK] ")
+if choice == "EUR" or choice == "CZK":
+	amount = input(f"How much of it? ")
+
+	if choice == "EUR":
+		print(f"Your amount is \033[1m{(int(amount) * rate):.3}\033[0m CZK")
+	elif choice == "CZK":
+		print(f"Your amount is \033[1m{(int(amount) / rate):.3}\033[0m EUR")
+else:
+	print("\033[1;31mDo a real currency!\033[0m")
+	sys.exit(65)
