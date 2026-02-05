5012dfb08cc855c8edf0c73273bb2540311b0151
MarekPlasek <marek.plasek.s@gyarab.cz>
2026-02-05 23:18:41 +0100

    completed homework: exchange, again

--- kurzy/exchange.py
+++ kurzy/exchange.py
@@ -22,6 +22,9 @@ print("\033[1mCurrency exchange thingy\033[0m")
 choice = input("What currency ya wanna convert? [EUR/CZK] ")
 if choice == "EUR" or choice == "CZK":
 	amount = input(f"How much of it? ")
+	if (int(amount) < 0):
+		print("\033[0;31mEnter a \033[1mnon-negative number\033[0m")
+		sys.exit(65)
 
 	if choice == "EUR":
 		print(f"Your amount is \033[1m{(int(amount) * rate):.3}\033[0m CZK")
